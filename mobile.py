a85b8a25b2481baa739b43b88cf4e1513d79a893
JD Gonzalez <jgonzalezp@costco.com>
2024-02-06 08:56:59 -0800

    add a wait till element is search available

--- mobile.py
+++ mobile.py
@@ -2,11 +2,13 @@ from uuid import uuid4
 from time import sleep
 from random import randint
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
 mobile_emulation = { "deviceName": "iPhone XR" }
@@ -16,23 +18,24 @@ options.add_argument("--user-data-dir=/home/josgon/.config/google-chrome")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 # Number of times to repeat the search process
-num_searches = 25
+num_searches = randint(21,26)
 
 # Loop to repeat the search process
 for i in range(num_searches):
-    # Generate a random GUID
-    search_query = str(uuid4().hex)[randint(0,18):randint(18,35)]
-    print(len(search_query), search_query)
-
     # Navigate to Bing.com
     driver.get('http://www.bing.com')
 
     # Find the search input element by its name attribute
-    search = driver.find_element(by=By.NAME, value="q")
+    search = WebDriverWait(driver, 30).until(EC.presence_of_element_located(by=By.NAME, value="q"))
 
-    # Enter the search query    
+    # Generate a random GUID
+    search_query = str(uuid4().hex)[randint(0,18):randint(18,35)]
+    print(len(search_query), search_query)
+
+    # Enter the search query
     search.send_keys(search_query)
-    
+    sleep(1)
+
     # Submit the search form
     search.send_keys(Keys.RETURN)
     sleep(randint(5,8))
