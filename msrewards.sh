e276b147f97bc5f48d79da8c97644406ada62e39
JD Gonzalez <jgonzalezp@costco.com>
2024-04-03 07:09:37 -0700

    Generalized path to scripts

--- msrewards.sh
+++ msrewards.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 read -p "Please define a sleep interval: " user_input
-python3 /home/josgon/msrewards/web.py "$user_input"
+cwd=$(dirname "$0")
+python3 $cwd/web.py "$user_input"
 sleep 900
-python3 /home/josgon/msrewards/mobile.py "$user_input"
+python3 $cwd/mobile.py "$user_input"
