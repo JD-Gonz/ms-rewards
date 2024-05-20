9380bdbf0b7515f624c6b97514e0472b10b9feba
JD Gonzalez <jgonzalezp@costco.com>
2024-05-20 10:14:51 -0700

    Add message between python scripts

--- msrewards.sh
+++ msrewards.sh
@@ -3,5 +3,6 @@
 read -p "Please define a sleep interval: " user_input
 cwd=$(dirname "$0")
 python3 $cwd/web.py "$user_input"
+echo "Completed web searches. Going to sleep for 15 min"
 sleep 900
 python3 $cwd/mobile.py "$user_input"
