3b26ba36bda253d7f1e5413e40507cbb77e6cf10
JD Gonzalez <jgonzalezp@costco.com>
2024-05-20 14:31:19 -0700

    make wait between scripts dynamic

--- msrewards.sh
+++ msrewards.sh
@@ -3,6 +3,6 @@
 read -p "Please define a sleep interval: " user_input
 cwd=$(dirname "$0")
 python3 $cwd/web.py "$user_input"
-echo "Completed web searches. Going to sleep for 15 min"
-sleep 900
+echo "Completed web searches. Going to sleep for $user_input min"
+sleep $(($user_input))m
 python3 $cwd/mobile.py "$user_input"
